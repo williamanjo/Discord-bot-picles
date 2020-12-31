e1e9bf3a1d6ce50dade096c9ff40f77ef5f9f2fd
WILLIAM OLIVEIRA DA SILVA <69880957+williamanjo@users.noreply.github.com>
2020-12-31 10:16:14 -0300

    Update id.js

--- commands/id.js
+++ commands/id.js
@@ -3,5 +3,5 @@ const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
   const pessoa = args.join(' ');
-  message.channel.send("ID: " +  message.mentions.users.first().id +  message.mentions.users.first().name)
+  message.channel.send("ID: " +  message.mentions.users.first().id +  message.mentions.users.first().username)
 };
