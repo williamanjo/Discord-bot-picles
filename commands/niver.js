cd64bdbde173a1569c30f9ba148126a29cb827a5
WILLIAM SILVA <william.anjodaguarda20@gmail.com>
2021-05-19 23:38:18 -0300

    Update niver.js

--- commands/niver.js
+++ commands/niver.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
   const pessoa = args.join(' ');
-  if (pessoa.toLowerCase() == "picolas"){message.channel.send("O aniversário de Picolas é 30/06/1998 !");}
+  if (message.mentions.users.first().id == "346059379843530752"){message.channel.send("O aniversário de Picolas é 30/06/1998 !");}
   else if (pessoa.toLowerCase() == "niko"){message.channel.send("O aniversário de Niko é 11/02/99 !");}
   else if (pessoa.toLowerCase() == "bellz"){message.channel.send("O aniversário de Bellz é 13/12/1997 !");}
   else if (pessoa.toLowerCase() == "júlio"){message.channel.send("O aniversário de Júlio é  22/08/2006 !");}
