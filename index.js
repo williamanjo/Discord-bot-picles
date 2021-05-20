a809c6ec0fb73f3e9d5098cbbfeab66569005a77
WILLIAM SILVA <william.anjodaguarda20@gmail.com>
2021-05-20 01:21:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ client.on('message', message => {
 client.on("ready", () => { 
   
   var date = moment("20/05/2021","DD/MM/YYYY");
-  client.channels.cache.get(birthdaychannel).send("birthday test : "+ date);
-  if(moment().format("DD/MM/YYYY") == date ){
-  client.channels.cache.get(birthdaychannel).send("birthday test : "+ date);
+  client.channels.cache.get(birthdaychannel).send("birthday test : "+ date.format("DD/MM/YYYY");
+  if(moment().format("DD/MM/YYYY") == date.format("DD/MM/YYYY") ){
+  client.channels.cache.get(birthdaychannel).send("birthday test : "+ date.format("DD/MM/YYYY");
 }});
    
 
