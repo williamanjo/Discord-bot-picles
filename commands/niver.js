837008d34900851f99dfe8337db4396087a2183d
WILLIAM SILVA <william.anjodaguarda20@gmail.com>
2021-05-19 23:53:36 -0300

    Update niver.js

--- commands/niver.js
+++ commands/niver.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
   const pessoa = args.join(' ');
-  if (message.mentions.users.first().id == 346059379843530752){message.channel.send("O aniversário de Picolas é 30/06/1998 !");}
-  else if (pessoa.toLowerCase() == "niko"){message.channel.send("O aniversário de Niko é 11/02/99 !");}
-  else if (pessoa.toLowerCase() == "bellz"){message.channel.send("O aniversário de Bellz é 13/12/1997 !");}
-  else if (pessoa.toLowerCase() == "júlio"){message.channel.send("O aniversário de Júlio é  22/08/2006 !");}
-  else if (pessoa.toLowerCase() == "blaiizer"){message.channel.send("O aniversário de Blaiizer é  27/02/2002 !");}
-  else { message.channel.send("Que pena não esta cadastrado esse usuario , Mande ao Administrador o ID ("+"ID ("+ message.mentions.users.first().username +"): " + message.mentions.users.first().id+").")}
+  if (message.mentions.users.first().id == 346059379843530752){message.channel.send("O aniversário de "+ message.mentions.users.first().username +" é 30/06/1998 !");}
+  else if (message.mentions.users.first().id  == 273583376672489472){message.channel.send("O aniversário de "+ message.mentions.users.first().username +" é 11/02/99 !");}
+  else if (message.mentions.users.first().id == "bellz"){message.channel.send("O aniversário de "+ message.mentions.users.first().username +" é 13/12/1997 !");}
+  else if (pessoa.toLowerCase() == "júlio"){message.channel.send("O aniversário de "+ message.mentions.users.first().username +" é  22/08/2006 !");}
+  else if (message.mentions.users.first().id == 201013211493957632){message.channel.send("O aniversário de "+ message.mentions.users.first().username +" é  27/02/2002 !");}
+  else { message.channel.send("Que pena não esta cadastrado esse usuario , Mande ao Administrador o  ID ("+ message.mentions.users.first().username +"): " + message.mentions.users.first().id+").")}
   
 };
