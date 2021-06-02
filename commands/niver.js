ea834d88410d8ab1133528029a78d210d9976f57
WILLIAM SILVA <william.anjodaguarda20@gmail.com>
2021-06-02 19:16:05 -0300

    Update niver.js

--- commands/niver.js
+++ commands/niver.js
@@ -7,6 +7,7 @@ module.exports.run = async (client, message, args) => {
   else if (message.mentions.users.first().id == 474413371874213889){message.channel.send("O aniversário de "+ message.mentions.users.first().username +" é 13/12/1997 !");}
   else if (pessoa.toLowerCase() == "júlio"){message.channel.send("O aniversário de "+ message.mentions.users.first().username +" é  22/08/2006 !");}
   else if (message.mentions.users.first().id == 201013211493957632){message.channel.send("O aniversário de "+ message.mentions.users.first().username +" é  27/02/2002 !");}
+  else if (message.mentions.users.first().id == 242401260609863701){message.channel.send("O aniversário de "+ message.mentions.users.first().username +" é  01/10/1999 !");}
   else if (message.mentions.users.first().id == 768238096532635648){message.channel.send("O Meu aniversário é  31/12/2020 !");}
   else { message.channel.send("Que pena não esta cadastrado esse usuario , Mande ao Administrador o  ID ("+ message.mentions.users.first().username +"): " + message.mentions.users.first().id+").")}
   
